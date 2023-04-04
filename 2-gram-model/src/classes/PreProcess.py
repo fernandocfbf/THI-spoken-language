3d0a8752bae9e70cd92c658d326487bbc5789f28
fernandocfbf <fernandocfbf@al.insper.edu.br>
2023-04-04 15:05:35 +0200

    feat: adding preprocess to remove stop words

--- 2-gram-model/src/classes/PreProcess.py
+++ 2-gram-model/src/classes/PreProcess.py
@@ -2,7 +2,7 @@
 import string
 
 #constants
-from src.constants.preprocess import USELESS_WORDS, NORMALIZATION_RULES
+from src.constants.preprocess import USELESS_WORDS, NORMALIZATION_RULES, STOP_WORDS
 
 class PreProcess():
     
@@ -17,9 +17,15 @@ class PreProcess():
         text = text.translate(str.maketrans('', '', string.digits))
         return text
 
+    def remove_stop_words(self, text):
+        words = text.split()
+        filtered_words = [word for word in words if word.lower() not in STOP_WORDS]
+        filtered_text = " ".join(filtered_words)
+
     def lemmatize_word(word):
         for suffix, replacement in NORMALIZATION_RULES.items():
             if word.endswith(suffix):
                 word = word[:-len(suffix)] + replacement
                 break
         return word
+    
